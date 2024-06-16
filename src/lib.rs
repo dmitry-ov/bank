ab8d7fec54d0991fb1a751a677ff7db85a0295a3
Dmitrii Ovcharenko <ovcharenkodmitry@gmail.com>
2024-06-17 02:22:49 +0300

    separate add\sub balance and logging history

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub struct Bank {
     // Счета
     accounts: HashSet<String>,
     // Балансы
-    balances: HashMap<String, u32>,
+    balances: HashMap<String, i32>,
     // История счета
     account_operations_index: HashMap<String, Vec<OperationId>>,
     // История
@@ -18,16 +18,16 @@ pub struct Bank {
 #[derive(Debug)]
 pub enum Operation {
     CreateAccount(String),
-    IncreaseAccount(String, u32),
-    DecreaseAccount(String, u32),
-    Transfer(String, String, u32),
+    IncreaseAccount(String, i32),
+    DecreaseAccount(String, i32),
+    Transfer(String, String, i32),
 }
 
 #[derive(Debug)]
 pub enum BankError {
     AccountAlreadyExists(String),
-    IncorrectAmount(u32),
-    InsufficientFunds(u32),
+    IncorrectAmount(i32),
+    InsufficientFunds(i32),
     TransferToMyself,
 }
 
@@ -39,6 +39,16 @@ impl Bank {
             account_operations_index: HashMap::new(),
             history: Vec::new(),
         }
+    }
+
+    pub fn get_account_balance(&self, account: String) -> Result<i32, BankError> {
+        if !self.accounts.contains(&account) {
+            return Err(AccountAlreadyExists(
+                format!(
+                    "Account {} does not exist", account)));
+        }
+        let balance = *self.balances.get(&account).unwrap();
+        Ok(balance)
     }
 
     pub fn create_account(&mut self, account: String) -> Result<(), BankError> {
@@ -54,39 +64,32 @@ impl Bank {
         }
     }
 
-    pub fn get_account_balance(&self, account: String) -> Result<u32, BankError> {
-        if !self.accounts.contains(&account) {
-            return Err(AccountAlreadyExists(
-                format!(
-                    "Account {} does not exist", account)));
-        }
-        let balance = self.balances.get(&account).unwrap();
-        Ok(*balance)
-    }
-
-    pub fn increase_account(&mut self, account: String, amount: u32) -> Result<(), BankError> {
-        if !self.accounts.contains(&account) {
-            return Err(AccountAlreadyExists(
-                format!(
-                    "Account {} does not exist", account)));
-        }
-
-        if amount == 0 {
-            return Err(BankError::IncorrectAmount(amount));
-        }
-
-        let current_balance = self.balances.get(&account).unwrap();
-        let new_balance = *current_balance + amount;
-        self.balances.insert(account.clone(), new_balance);
+    pub fn increase_account(&mut self, account: String, amount: i32) -> Result<(), BankError> {
+        self.update_balance_account(account.clone(), amount)?;
         self.history.push(Operation::IncreaseAccount(account, amount));
         Ok(())
     }
 
-    pub fn decrease_account(&mut self, account: String, amount: u32) -> Result<(), BankError> {
+    pub fn decrease_account(&mut self, account: String, amount: i32) -> Result<(), BankError> {
+        self.update_balance_account(account.clone(), (-1) * amount)?;
+        self.history.push(Operation::DecreaseAccount(account, amount));
+        Ok(())
+    }
+
+    pub fn transfer(&mut self, from: String, to: String, amount: i32) -> Result<(), BankError> {
+        if from == to {
+            Err(BankError::TransferToMyself)
+        } else {
+            self.update_balance_account(from.clone(), (-1) * amount)?;
+            self.update_balance_account(to.clone(), amount)?;
+            self.history.push(Operation::Transfer(from, to, amount));
+            Ok(())
+        }
+    }
+
+    fn update_balance_account(&mut self, account: String, amount: i32) -> Result<(), BankError> {
         if !self.accounts.contains(&account) {
-            return Err(AccountAlreadyExists(
-                format!(
-                    "Account {} does not exist", account)));
+            return Err(AccountAlreadyExists(format!("Account {} does not exist", account)));
         }
 
         if amount == 0 {
@@ -94,25 +97,14 @@ impl Bank {
         }
 
         let current_balance = *self.balances.get(&account).unwrap();
-        if current_balance < amount {
+        let new_balance = current_balance + amount;
+
+        if new_balance < 0 {
             return Err(BankError::InsufficientFunds(amount));
         }
 
-        let new_balance = current_balance - amount;
         self.balances.insert(account.clone(), new_balance);
-        self.history.push(Operation::DecreaseAccount(account, amount));
         Ok(())
-    }
-
-    pub fn transfer(&mut self, from: String, to: String, amount: u32) -> Result<(), BankError> {
-        if from == to {
-            Err(BankError::TransferToMyself)
-        } else {
-            self.decrease_account(from.clone(), amount)?;
-            self.increase_account(to.clone(), amount)?;
-            self.history.push(Operation::Transfer(from, to, amount));
-            Ok(())
-        }
     }
 
     pub fn get_history(&self) -> &Vec<Operation> {
@@ -274,7 +266,7 @@ mod tests {
         let history = bank.get_history();
         assert_eq!(1, history.len());
         // assert_eq!(3, history.len());
-        println!( "{:?}",history)
+        println!("{:?}", history)
     }
 }
 
